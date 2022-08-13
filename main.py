0049cbe23fb28e27736333290996f5b6b7822504
Allan Cao <a33cao@uwaterloo.ca>
2022-08-13 04:13:41 -0400

    add boilerplate discord code

--- main.py
+++ main.py
@@ -2,17 +2,27 @@ import discord
 import os
 from dotenv import load_dotenv
 import sqlite3
+load_dotenv() # Loads .env variables
 
-connection = sqlite3.connect("userbase.db", isolation_level=None)
+### Setup SQL (sqlite3 for now but could use something better in the future)
+DATABASE_FILE = os.environ.get('DB_FILE')
+connection = sqlite3.connect(DATABASE_FILE, isolation_level=None)
 cursor = connection.cursor()
 
-load_dotenv()
+### Setup Discord Bot
+intents = discord.Intents.default()
 BOT_TOKEN = os.environ.get('SERA_TOKEN')
+bot = discord.Client(intents = intents) # Most functions require intents (will require discord approval at 100 members)
 
-client = discord.Client()
-
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
+    await bot.change_presence(
+        status=discord.Status.online,
+        activity=discord.Game("Inhouse Queue"))
 
-client.run(BOT_TOKEN)
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+
+bot.run(BOT_TOKEN)
