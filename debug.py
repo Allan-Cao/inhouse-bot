6f350f873e9c4993607839c521ab220451560fdb
Allan Cao <a33cao@uwaterloo.ca>
2022-08-13 14:51:49 -0400

    add test user creation code

--- debug.py
+++ debug.py
@@ -3,26 +3,36 @@
 
 import os
 from dotenv import load_dotenv
-import sqlite3
-
-load_dotenv() # Loads .env variables
 import csv
+load_dotenv() # Loads .env variables
 
-### Setup SQL (sqlite3 for now but could use something better in the future)
-DATABASE_FILE = os.environ.get('DB_FILE')
-connection = sqlite3.connect(DATABASE_FILE, isolation_level=None)
-cursor = connection.cursor()
+### Setup SQL
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(host=os.getenv('SQL_URL'),
+                                         database='inhouse', #CREATE DATABASE inhouse
+                                         user=os.getenv('SQL_USER'),
+                                         password=os.getenv('SQL_PWD'))
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+except Error as e:
+    print("Error while connecting to MySQL", e)
 
 # Create Initial Database
-cursor.execute("""CREATE TABLE users (
-    ign TEXT,
-    id INTEGER,
-    main_role INTEGER,
-    secondary_role INTEGER,
-    fill_queue BOOLEAN,
-    elo INTEGER,
-    timeout INTEGER)
-""")
+
+# cursor.execute("""CREATE TABLE users (
+#     ign TEXT,
+#     id BIGINT,
+#     main_role ENUM('Top','Jungle','Mid','ADC','Support'),
+#     secondary_role ENUM('Top','Jungle','Mid','ADC','Support'),
+#     fill_queue BOOLEAN,
+#     elo INTEGER,
+#     timeout INTEGER)
+# """)
 
 # Roles: Top, Jungle, Mid, ADC, Support => 0,1,2,3,4
 
@@ -40,10 +50,19 @@ cursor.execute("""CREATE TABLE users (
 
 ### Test Users
 
-cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)",
-            ("TauPiPhi", 0, 4, 0, False, 1000, 0)
-)
+user_insert_query = """
+INSERT INTO users
+(ign, id, main_role, secondary_role, fill_queue, elo, timeout)
+VALUES ( %s, %s, %s, %s, %s, %s, %s )
+"""
 
-cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)",
-            ("DravenMain1", 0, 3, 0, False, 1000, 0)
-)
+test_users = [
+#    ("TauPiPhi", 645940845245104130, 'Support', None, False, 1000, 0),
+#    ("DravenMain1", 0, 'ADC', None, False, 1000, 0),
+#    ("Âzîr", 0, 'Mid', None, False, 1000, 0)
+    # ("Manì", 0, 'ADC', None, False, 1000, 0)
+]
+
+with connection.cursor() as cursor:
+    cursor.executemany(user_insert_query, test_users)
+    connection.commit()
