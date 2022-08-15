0432f78dc679fdc5057a9b9db7bb816b06d7bfa5
Allan Cao <a33cao@uwaterloo.ca>
2022-08-14 22:31:58 -0400

    update library links in refractored codebase

--- inhouse_bot/debug.py
+++ inhouse_bot/debug.py
@@ -4,7 +4,8 @@
 import os
 from dotenv import load_dotenv
 import csv
-load_dotenv() # Loads .env variables
+
+load_dotenv()  # Loads .env variables
 from inhouse import *
 
 ### Setup SQL
@@ -12,10 +13,12 @@ import mysql.connector
 from mysql.connector import Error
 
 try:
-    connection = mysql.connector.connect(host=os.getenv('SQL_URL'),
-                                         database='inhouse', #CREATE DATABASE inhouse
-                                         user=os.getenv('SQL_USER'),
-                                         password=os.getenv('SQL_PWD'))
+    connection = mysql.connector.connect(
+        host=os.getenv("SQL_URL"),
+        database="inhouse",  # CREATE DATABASE inhouse
+        user=os.getenv("SQL_USER"),
+        password=os.getenv("SQL_PWD"),
+    )
     if connection.is_connected():
         db_Info = connection.get_server_info()
         print("Connected to MySQL Server version ", db_Info)
@@ -42,19 +45,21 @@ except Error as e:
 # VALUES ( %s, %s, %s, %s, %s, %s, %s )
 # """
 
-cursor.execute("""CREATE TABLE ready_check (
+cursor.execute(
+    """CREATE TABLE ready_check (
     ign TEXT,
     id BIGINT,
     role ENUM('Top','Jungle','Mid','ADC','Support'),
     elo FLOAT(1),
     game_id INT,
     status BOOLEAN)
-""")
+"""
+)
 
 test_users = [
-#    ("TauPiPhi", 645940845245104130, 'Support', None, False, 1000, 0),
-#    ("DravenMain1", 0, 'ADC', None, False, 1000, 0),
-#    ("Âzîr", 0, 'Mid', None, False, 1000, 0)
+    #    ("TauPiPhi", 645940845245104130, 'Support', None, False, 1000, 0),
+    #    ("DravenMain1", 0, 'ADC', None, False, 1000, 0),
+    #    ("Âzîr", 0, 'Mid', None, False, 1000, 0)
     # ("Manì", 0, 'ADC', None, False, 1000, 0)
 ]
 
--- inhouse_bot/inhouse_bot.py
+++ inhouse_bot/inhouse_bot.py
@@ -3,12 +3,14 @@ from discord.ext import commands
 from dotenv import load_dotenv
 load_dotenv() # Loads .env variables
 import os
+
 intents = discord.Intents.default()
 test_servers = [821109702162907158]
 
+
 class InhouseBot(commands.Bot):
     def __init__(self):
-        super().__init__(intents=intents, debug_guilds= test_servers)
+        super().__init__(intents=intents, debug_guilds=test_servers)
         from inhouse_bot.cogs.queue_cog import queue_cog
         from inhouse_bot.cogs.admin_cog import admin_cog
         from inhouse_bot.cogs.info_cog import info_cog
@@ -23,6 +25,8 @@ class InhouseBot(commands.Bot):
         print(f"We have logged in as {self.user.name}")
         await self.change_presence(
             status=discord.Status.online,
-            activity=discord.Game("Managing Inhouse Queues"))
+            activity=discord.Game("Managing Inhouse Queues"),
+        )
+
     def run(self):
-        super().run(os.getenv('SERA_TOKEN'))
+        super().run(os.getenv("SERA_TOKEN"))
